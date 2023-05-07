8116d124d6a44a7270ca852ddf742ef375560a3b
Андрей Голишевский <agolishevskiy@yandex.ru>
2023-05-07 19:29:16 +0300

    realize ingredient's responses

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
+from recipes.models import Ingredient
 
-# Create your views here.
+from rest_framework.viewsets import ReadOnlyModelViewSet
+
+from .filters import IngredientFilter
+from .permissions import IsAdminOrReadOnly
+from .serializers import IngredientSerializer
+
+
+class IngredientViewSet(ReadOnlyModelViewSet):
+    queryset = Ingredient.objects.all()
+    serializer_class = IngredientSerializer
+    permission_classes = (IsAdminOrReadOnly,)
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = IngredientFilter

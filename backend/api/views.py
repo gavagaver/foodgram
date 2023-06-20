1f0285a677a0a64d7d9e6a73fe98f26e987de271
Андрей Голишевский <agolishevskiy@yandex.ru>
2023-06-21 00:31:23 +0300

    add actions

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,11 @@
+from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
-from recipes.models import Ingredient, Tag, Recipe
+from recipes.models import Ingredient, Tag, Recipe, Favorite, ShoppingCart
 from foodgram.pagination import CustomPagination
+from rest_framework import status
+from rest_framework.decorators import action
 from rest_framework.permissions import SAFE_METHODS
+from rest_framework.response import Response
 
 from rest_framework.viewsets import ReadOnlyModelViewSet, ModelViewSet
 
@@ -38,3 +42,33 @@ class RecipeViewSet(ModelViewSet):
         if self.request.method in SAFE_METHODS:
             return RecipeReadSerializer
         return RecipeWriteSerializer
+
+    @action(
+        detail=True,
+        url_path='favorite',
+        methods=('POST', 'DELETE'),
+    )
+    def favorite(self, request, pk):
+        recipe = get_object_or_404(Recipe, pk=pk)
+        if request.method == 'DELETE':
+            Favorite.objects.filter(user=request.user, recipe=recipe).delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+        Favorite.objects.create(user=request.user, recipe=recipe)
+        serializer = RecipeReadSerializer(recipe)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    @action(
+        detail=True,
+        url_path='shopping_cart',
+        methods=('POST', 'DELETE'),
+    )
+    def shopping_cart(self, request, pk):
+        recipe = get_object_or_404(Recipe, pk=pk)
+        if request.method == 'DELETE':
+            ShoppingCart.objects.filter(user=request.user, recipe=recipe).delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+        ShoppingCart.objects.create(user=request.user, recipe=recipe)
+        serializer = RecipeReadSerializer(recipe)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,10 +1,16 @@
 from django.contrib.auth import get_user_model
+from django.shortcuts import get_object_or_404
 from djoser.views import UserViewSet
 from foodgram.pagination import CustomPagination
+from rest_framework import status
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.response import Response
+
+from models import CustomUser, Subscribe
 from .serializers import CustomUserSerializer
 
-User = get_user_model()
+User = CustomUser()
 
 
 class CustomUserViewSet(UserViewSet):
@@ -12,3 +18,18 @@ class CustomUserViewSet(UserViewSet):
     serializer_class = CustomUserSerializer
     permission_classes = (IsAuthenticatedOrReadOnly,)
     pagination_class = CustomPagination
+
+    @action(
+        detail=True,
+        url_path='subscribe',
+        methods=('POST', 'DELETE'),
+    )
+    def subscribe(self, request, pk):
+        author = get_object_or_404(User, pk=pk)
+        if request.method == 'DELETE':
+            Subscribe.objects.filter(user=request.user, author=author).delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+        Subscribe.objects.create(user=request.user, author=author)
+        serializer = CustomUserSerializer(author)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)

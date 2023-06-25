f9a64af55ac78457f3296a8b38e21313d4d82893
Андрей Голишевский <agolishevskiy@yandex.ru>
2023-06-25 15:09:23 +0300

    add custom permission

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -1,6 +1,15 @@
 from rest_framework.permissions import SAFE_METHODS, BasePermission
 
 
+class IsAuthorOrAdminOrReadOnly(BasePermission):
+
+    def has_permission(self, request, view):
+        return request.method in SAFE_METHODS or request.user.is_staff
+
+    def has_object_permission(self, request, view, obj):
+        return obj.author == request.user
+
+
 class IsAdminOrReadOnly(BasePermission):
 
     def has_permission(self, request, view):

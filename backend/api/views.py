3e05cfb23fce507e529920ff39be91b5969516d7
Андрей Голишевский <agolishevskiy@yandex.ru>
2023-06-25 15:07:37 +0300

    add download shopping cart

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,25 +1,27 @@
+from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
-from recipes.models import Ingredient, Tag, Recipe, Favorite, ShoppingCart
+from recipes.models import Ingredient, Tag, Recipe, Favorite, ShoppingCart, RecipeIngredient
 from foodgram.pagination import CustomPagination
 from rest_framework import status
 from rest_framework.decorators import action
-from rest_framework.permissions import SAFE_METHODS
+from rest_framework.permissions import SAFE_METHODS, IsAuthenticated, \
+    IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 
 from rest_framework.viewsets import ReadOnlyModelViewSet, ModelViewSet
 
 from .filters import IngredientFilter, RecipeFilter
-from .permissions import IsAdminOrReadOnly
-from .serializers import IngredientSerializer, TagSerializer, RecipeReadSerializer, RecipeWriteSerializer
+from .permissions import IsAuthorOrAdminOrReadOnly, IsAdminOrReadOnly
+from .serializers import IngredientSerializer, TagSerializer, RecipeReadSerializer, RecipeWriteSerializer, RecipeCardSerializer
 
 
 class IngredientViewSet(ReadOnlyModelViewSet):
     queryset = Ingredient.objects.all()
     serializer_class = IngredientSerializer
-    permission_classes = (IsAdminOrReadOnly,)
     filter_backends = (DjangoFilterBackend,)
     filterset_class = IngredientFilter
+    permission_classes = (IsAdminOrReadOnly,)
 
 
 class TagViewSet(ReadOnlyModelViewSet):
@@ -30,7 +32,7 @@ class TagViewSet(ReadOnlyModelViewSet):
 
 class RecipeViewSet(ModelViewSet):
     queryset = Recipe.objects.all()
-    permission_classes = (IsAdminOrReadOnly,)
+    permission_classes = (IsAuthorOrAdminOrReadOnly,)
     pagination_class = CustomPagination
     filter_backends = (DjangoFilterBackend,)
     filterset_class = RecipeFilter
@@ -55,7 +57,7 @@ class RecipeViewSet(ModelViewSet):
             return Response(status=status.HTTP_204_NO_CONTENT)
 
         Favorite.objects.create(user=request.user, recipe=recipe)
-        serializer = RecipeReadSerializer(recipe)
+        serializer = self.get_serializer_class()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     @action(
@@ -70,5 +72,50 @@ class RecipeViewSet(ModelViewSet):
             return Response(status=status.HTTP_204_NO_CONTENT)
 
         ShoppingCart.objects.create(user=request.user, recipe=recipe)
-        serializer = RecipeReadSerializer(recipe)
+        serializer = RecipeCardSerializer(
+            recipe,
+            context={'request': request}
+        )
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    @action(
+        detail=False,
+        url_path='download_shopping_cart',
+        methods=('GET',),
+        permission_classes=(IsAuthenticated,)
+    )
+    def download_shopping_cart(self, request):
+        user = request.user
+        recipes_in_cart = user.shopping_cart.all()
+
+        total_amounts = {}
+        for recipe in recipes_in_cart:
+            ingredients = RecipeIngredient.objects.filter(
+                recipe=recipe,
+            )
+            for ingredient in ingredients:
+                ingredient_label = (
+                    f'{ingredient.name} ({ingredient.measurement_unit})'
+                )
+                ingredient_amount = ingredient.amount
+                total_amounts[ingredient_label] = (
+                        total_amounts.get(ingredient_label, 0)
+                        + ingredient_amount
+                )
+
+        list_of_ingredients = []
+        for ingredient_label, amount in total_amounts.items():
+            list_of_ingredients.append(
+                f'{ingredient_label} - {amount}'
+            )
+
+        header = 'Ваш список покупок' + '-------------------\n'
+        footer = '-------------------\n' + 'Составлено в foodgram'
+        response = HttpResponse(
+            header + '\n' + '\n'.join(list_of_ingredients) + '\n' + footer,
+            content_type='text/plain',
+        )
+        response['Content-Disposition'] = (
+            'attachment; filename=Shopping_Cart.txt'
+        )
+        return response

7be2d3fcff41391c31cfbafcba75c461fc34856e
Андрей Голишевский <agolishevskiy@yandex.ru>
2023-06-26 21:30:29 +0300

    correct by flake8

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,11 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import status
-from rest_framework.decorators import action
-from rest_framework.permissions import SAFE_METHODS, IsAuthenticated
-from rest_framework.response import Response
-from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 from foodgram.pagination import CustomPagination
 from recipes.models import (
     Favorite,
@@ -15,6 +10,11 @@ from recipes.models import (
     ShoppingCart,
     Tag,
 )
+from rest_framework import status
+from rest_framework.decorators import action
+from rest_framework.permissions import SAFE_METHODS, IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 
 from .filters import IngredientFilter, RecipeFilter
 from .permissions import IsAdminOrReadOnly, IsAuthorOrAdminOrReadOnly

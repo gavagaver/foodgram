d6b621e269cab30aade550691fc7f310690ab003
Андрей Голишевский <agolishevskiy@yandex.ru>
2023-06-26 12:14:32 +0300

    correct viewset name

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,13 +1,13 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import UserViewSet
+from .views import CustomUserViewSet
 
 app_name = 'users'
 
 router = DefaultRouter()
 
-router.register('users', UserViewSet)
+router.register('users', CustomUserViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),

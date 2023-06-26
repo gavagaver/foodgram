ff1ec8f764f4afe2b39dd3e3554e979e62e24b74
Андрей Голишевский <agolishevskiy@yandex.ru>
2023-06-26 12:16:00 +0300

    update IsAuthorOrAdminOrReadOnly

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -3,11 +3,12 @@ from rest_framework.permissions import SAFE_METHODS, BasePermission
 
 class IsAuthorOrAdminOrReadOnly(BasePermission):
 
-    def has_permission(self, request, view):
-        return request.method in SAFE_METHODS or request.user.is_staff
-
     def has_object_permission(self, request, view, obj):
-        return obj.author == request.user
+        return (
+            request.method in SAFE_METHODS
+            or request.user.is_staff
+            or obj.author == request.user
+        )
 
 
 class IsAdminOrReadOnly(BasePermission):

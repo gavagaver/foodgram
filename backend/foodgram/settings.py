763871cce1329ea86e91be1501c8525ee05dd226
Андрей Голишевский <agolishevskiy@yandex.ru>
2023-06-27 23:58:39 +0300

    change default od secret key

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', default='48ksll%385148ksll%385148ksll%385148')
+SECRET_KEY = os.getenv('SECRET_KEY', default='django-insecure-qehnknfu!hay&*9un==(!$=o54xre$7q&^j1=b4(vwmc$jv55+')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
